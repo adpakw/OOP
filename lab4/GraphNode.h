655926b37bf9cb09d2cae5b618509817dc52eda5
adpakw <adpak@gmail.com>
2023-12-16 04:03:41 +0300

    Fix Lab4

--- lab4/GraphNode.h
+++ lab4/GraphNode.h
@@ -4,86 +4,75 @@
 #include <iostream>
 #include <vector>
 
-template <typename K, typename V>
-class GraphNode {
+template <typename Key, typename Value> class GraphNode {
 private:
-    K key;
-    V value;
+  Key key;
+  Value value;
 
-    std::vector<GraphNode<K, V>> nodes;
-    std::vector<int> costs;
+  std::vector<GraphNode<Key, Value>> nodes;
+  std::vector<int> costs;
+
 public:
+  Key getKey() const { return key; }
 
-    K getKey() const{
-        return key;
+  void setKey(const Key &value) { this->key = value; }
+
+  Value getValue() const { return value; }
+
+  void setValue(const Value &value) { this->value = value; }
+
+  std::vector<GraphNode<Key, Value>> getNodes() const { return nodes; }
+
+  std::vector<int> getCosts() const { return costs; }
+
+  GraphNode() {
+    key = Key();
+    value = Value();
+
+    nodes = std::vector<GraphNode<Key, Value>>();
+    costs = std::vector<int>();
+  }
+
+  GraphNode(const Key &key, const Value &value) {
+    this->key = key;
+    this->value = value;
+
+    nodes = std::vector<GraphNode<Key, Value>>();
+    costs = std::vector<int>();
+  }
+
+  void addConnection(const GraphNode<Key, Value> &graphNode, int cost) {
+    nodes.push_back(graphNode);
+    costs.push_back(cost);
+  }
+
+  bool operator==(const GraphNode<Key, Value> &graphNode) {
+    return key == graphNode.key && value == graphNode.value;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const GraphNode<Key, Value> &graphNode) {
+    os << graphNode.key << ' ' << graphNode.value << ' '
+       << graphNode.nodes.size() << ' ';
+    for (size_t i = 0; i < graphNode.nodes.size(); ++i) {
+      os << graphNode.nodes[i] << ' ' << graphNode.costs[i] << ' ';
     }
+    os << '\n';
+    return os;
+  }
 
-    void setKey(const K &value){
-        this->key = value;
+  friend std::istream &operator>>(std::istream &is,
+                                  GraphNode<Key, Value> &graphNode) {
+    is >> graphNode.key >> graphNode.value;
+    int nodeCount;
+    is >> nodeCount;
+    graphNode.nodes.resize(nodeCount);
+    graphNode.costs.resize(nodeCount);
+    for (int i = 0; i < nodeCount; ++i) {
+      is >> graphNode.nodes[i] >> graphNode.costs[i];
     }
-
-    V getValue() const{
-        return value;
-    }
-
-    void setValue(const V &value){
-        this->value = value;
-    }
-
-    std::vector<GraphNode<K, V>> getNodes() const{
-        return nodes;
-    }
-
-    std::vector<int> getCosts() const{
-        return costs;
-    }
-
-    GraphNode(){
-        key = K();
-        value = V();
-
-        nodes = std::vector<GraphNode<K, V>>();
-        costs = std::vector<int>();
-    }
-
-    GraphNode(const K &key, const V &value){
-        this->key = key;
-        this->value = value;
-
-        nodes = std::vector<GraphNode<K, V>>();
-        costs = std::vector<int>();
-    }
-
-    void addConnection(const GraphNode<K, V> &graphNode, int cost){
-        nodes.push_back(graphNode);
-        costs.push_back(cost);
-    }
-
-    bool operator == (const GraphNode<K, V> &graphNode){
-        return key==graphNode.key && value==graphNode.value;
-    }
-
-    friend std::ostream& operator<<(std::ostream &os, const GraphNode<K,V> &graphNode){
-        os << graphNode.key << ' ' << graphNode.value << ' ' << graphNode.nodes.size() << ' ';
-        for (size_t i = 0; i < graphNode.nodes.size(); ++i) {
-            os << graphNode.nodes[i] << ' ' << graphNode.costs[i] << ' ';
-        }
-        os << '\n';
-        return os;
-    }
-
-    friend  std::istream& operator>>(std::istream &is, GraphNode<K, V> &graphNode){
-        is >> graphNode.key >> graphNode.value;
-        int nodeCount;
-        is >> nodeCount;
-        graphNode.nodes.resize(nodeCount);
-        graphNode.costs.resize(nodeCount);
-        for (int i = 0; i < nodeCount; ++i) {
-            is >> graphNode.nodes[i] >> graphNode.costs[i];
-        }
-        return is;
-    }
+    return is;
+  }
 };
-
 
 #endif
--- lab4/PostOffice.h
+++ lab4/PostOffice.h
@@ -1,26 +1,25 @@
-
 #ifndef LAB4_POSTOFFICE_H
 #define LAB4_POSTOFFICE_H
-
 
 #include <istream>
 
 class PostOffice {
 private:
-    int index;
-    int mails;
+  int index;
+  int mails;
+
 public:
-    const int getIndex() const;
-    const int getMails() const;
+  const int getIndex() const;
+  const int getMails() const;
 
-    PostOffice();
-    PostOffice(int index, int mails);
+  PostOffice();
+  PostOffice(int index, int mails);
 
-    bool operator == (const PostOffice &postOffice) const;
+  bool operator==(const PostOffice &postOffice) const;
 
-    friend std::ostream& operator<<(std::ostream &os, const PostOffice &postOffice);
-    friend  std::istream& operator>>(std::istream &is, PostOffice &postOffice);
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const PostOffice &postOffice);
+  friend std::istream &operator>>(std::istream &is, PostOffice &postOffice);
 };
 
-
-#endif 
+#endif
